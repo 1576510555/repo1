a90931a0d44908e1a4a486181d7c479f845398ea
zhang <binfengzhang3306@163.com>
2021-05-09 17:07:41 +0800

    修改

--- sort/bubble/src/insertSort.java
+++ sort/bubble/src/insertSort.java
@@ -6,26 +6,32 @@ public class insertSort {
 
     public static void main(String[] args) {
 
-        int [] arr = {23,546,6,234,657,32,24,51,21};
+        int[] arr = {23, 546, 6, 234, 657, 32, 24, 51, 21};
 
         for (int i = 1; i < arr.length; i++) {
             int insertVal = arr[i];
-            int insertIndex = i-1;
-            while(insertIndex >= 0 && insertVal < arr[insertIndex]){
-                arr[insertIndex+1] = arr[insertIndex];
+            int insertIndex = i - 1;
+            while (insertIndex >= 0 && insertVal < arr[insertIndex]) {
+                arr[insertIndex + 1] = arr[insertIndex];
                 insertIndex--;
-            } arr[insertIndex+1] = insertVal;
+            }
+            arr[insertIndex + 1] = insertVal;
         }
         System.out.println(Arrays.toString(arr));
 
     }
-    public void main(){
+
+    public void main() {
         for (int i = 0; i < 100; i++) {
             System.out.println("hi");
         }
     }
-    public void eat(){
+
+    public void eat() {
 
     }
 
+    public void test() {
+
+    }
 }
